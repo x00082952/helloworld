06635ffd9c26cb1943bfe5ef9f4bd5bddea342cd
X00082952 <X00082952@computing.stu.it-tallaght.ie>
2016-10-18 11:32:22 +0100

    Fixed the spelling mistake in name.

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello
 {
 	public static void main(String [] args)
 	{
-		System.out.println("Hello Roburt.");
+		System.out.println("Hello Robert.");
 		System.out.println("Welcome.");
 	}
 }
